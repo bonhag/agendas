a8ec00b8d67f94793033598a2e08e43653acfd2d
Jeff Bonhag <bonhagjeffrey@gmail.com>
2013-05-15 00:02:44 -0400

    just a little joke

--- agendas.js
+++ agendas.js
@@ -1,21 +1,64 @@
-Agendas = new Meteor.Collection("agendas");
 Items = new Meteor.Collection("items");
 
 if (Meteor.isClient) {
   Template.agenda.date = function() {
-    return 'Wednesday, May 15'
+    return 'May 15, 2013'
   };
 
   Template.agenda.items = function () {
     return Items.find();
   };
 
-  Template.agenda.events({
-    'click button.previous': function () {
-      Session.set("current_agenda", Session.get("current_agenda") - 1);
+  Template.item.events({
+    'click i.icon-remove': function () {
+      Items.remove({_id: this._id});
+    }
+  });
+
+  var inflateEstimate = function (estimate) {
+    var splitEstimate = estimate.split(' ');
+    var minutes = splitEstimate[0];
+    var text = splitEstimate[1];
+    minutes *= 6;
+    return [minutes, text].join(' ');
+  };
+
+  var addNewItem = function () {
+    var owner = $("#owner").val();
+    var estimate = $("#estimate").val();
+
+    if (owner.toLowerCase() == 'chapin') {
+      estimate = inflateEstimate(estimate);
+    }
+
+    Items.insert({
+      description: $("#description").val(),
+      estimate: estimate,
+      owner: owner
+    });
+  };
+
+  var clearInputForm = function () {
+    $("#description").val("");
+    $("#estimate").val("");
+    $("#owner").val("");
+  };
+
+  var focusOnDescription = function () {
+    $("#description").focus();
+  };
+
+  Template.add.events({
+    'keypress': function(keyEvent) {
+      if (keyEvent.keyCode == 13) {
+        $('i.icon-plus').click();
+      }
     },
-    'click button.next': function () {
-      Session.set("current_agenda", Session.get("current_agenda") + 1);
+
+    'click i.icon-plus': function () {
+      addNewItem();
+      clearInputForm();
+      focusOnDescription();
     }
   });
 }
